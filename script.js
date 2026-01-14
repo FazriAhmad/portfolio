abe5fc5b3263d9a884c7dc3a1ce3eb211da40743
FazriAhmad <118627201+FazriAhmad@users.noreply.github.com>
2026-01-14 20:25:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// Show email function
+
+
+function InfoKontak() {
+  const contactInfo = `
+    Email:fazriahmadmustaqim@gmail.com<br>
+    Nomor HP: <a href="tel:+6281284020220">+62 812-8402-0220</a><br>
+  `;
+  document.getElementById("contactField").innerHTML = contactInfo;
+}
+
+
+// AOS init
+AOS.init();
+const toggleBtn = document.getElementById("toggleDark");
+const body = document.body;
+
+
 
